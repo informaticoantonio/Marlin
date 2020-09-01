f82153d083ec2793e2ff581f8abee7a5eadb3d86
antonio <informaticoantonio@gmail.com>
2020-09-01 08:59:37 +0200

    updated motor steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,7 +747,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.121, 80.121, 399.778, 397.6 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.6, 80.2, 401, 406 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -997,7 +997,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 30
+#define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
