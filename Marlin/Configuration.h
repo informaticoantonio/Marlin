9ed698cb9c6df6c3fb9d87c9aa10a4d8fa8e22f6
informaticoantonio <informaticoantonio@gmail.com>
2020-12-11 09:22:02 +0100

    M92 calibrated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.5, 79.5, 405, 406.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 83.8, 77, 401, 406.2 }
 
 /**
  * Default Max Feed Rate (mm/s)
