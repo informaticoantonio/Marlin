2045eab5d91b7e93f462a9661316a8a9086585f9
informaticoantonio <informaticoantonio@gmail.com>
2023-11-23 01:23:52 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1554,7 +1554,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -30, 10, -0.690 }
+#define NOZZLE_TO_PROBE_OFFSET { -30, 10, -0.685 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
