71fd291a07e58fa206a904d3a3953924ecebf401
informaticoantonio <informaticoantonio@gmail.com>
2023-11-26 14:01:51 +0100

    input shapping config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1234,7 +1234,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 50, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 120 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1554,7 +1554,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 25, -35, -0.833 }
+#define NOZZLE_TO_PROBE_OFFSET { 25, -35, -0.86 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
